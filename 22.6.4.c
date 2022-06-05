adfe47da25fe0e544b762c9ee932a9628fc59760
z1y2q6 <942320647@qq.com>
2022-06-05 16:35:46 +0800

    22.6.4初学c语言

---
+++ 22.6.4.c
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+//int main()
+//{
+//	int input;
+//	printf("谁是大帅比？1：吴彦祖 2：彭于晏 3：魏家涵>:");
+//	scanf("%d", &input);
+//	if (input == 3)
+//		printf("脸都不要了");
+//	else
+//	{
+//        printf("你再想想?\n");
+//		scanf("%d", &input);
+//		if (input == 3)
+//			printf("脸都不要了");
+//		else
+//		{
+//			printf("你再想想?\n");
+//			scanf("%d", &input);
+//			if (input == 3)
+//				printf("脸都不要了");
+//		}
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int sz[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		int a = sz[i];
+//			printf("%d,", a);
+//			i++;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 5;
+//	int c = 7;
+//	int d = (a = b + 2, c = a - 4);
+//	printf("%d\n", d);
+//	return 0;
+//}
